8754abd55fe7753322b8ebf739ae2e09e7e94412
Nick Cruickshank <64281712+ncruickshank@users.noreply.github.com>
2026-02-22 09:50:10 -0600

    create multi-lab nb and rename dataset py file

--- src/data_gathering/scryfall_dataset.py
+++ src/data_gathering/scryfall_dataset.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from datasets import load_dataset
 
-class ScryfallQADataset():
+class ScryfallDataset():
     """
     Description
     ----------
